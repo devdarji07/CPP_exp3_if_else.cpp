3a6849506689b0f9e42fb40c277d7aa6658b7bf2
Dev <devkdarji@gmail.com>
2025-07-24 19:45:41 +0530

    Create positive negative number finder.cpp

---
+++ positive negative number finder.cpp
@@ -0,0 +1,29 @@
+//Dev Darji
+//24070123033
+//Batch: A2
+
+#include <iostream>
+using namespace std;
+int a,b;
+
+int main() {
+    cout<< " Value of a: ";
+    cin >> a;
+    cout<< " Value of b: ";
+    cin >> b;
+    
+    if (a>0){
+        cout<< "A is a +ve number." << "\n";
+    }
+    else{
+        cout<< "A is a -ve number."<< "\n";
+    }
+    
+    if (b>0){
+        cout<< "B is a +ve number."<< "\n";
+    }
+    else{
+        cout<< "B is a -ve number."<< "\n";
+    }
+    
+    return 0;
